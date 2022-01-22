fc3d5c2c97d5198d116f406e52c9a2108e5c7688
Ben Sidelinger <ben_sidelinger@discovery.com>
2022-01-21 22:12:13 -0800

    web started and ran one

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const web3 = (window as any).AlchemyWeb3.createAlchemyWeb3('https://eth-ropsten.alchemyapi.io/v2/key');
+const contract = require("./MyNFT.json")
+const contractAddress = ""
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
+
 function App() {
+  const [accounts, setAccounts] = React.useState<any[]>();
+  const [metaData, setMetaData] = React.useState<string>();
+
+  const connectWallet = async () => {
+    const accounts = await (window as any).ethereum.request({ method: 'eth_requestAccounts' });
+    console.log(accounts);
+    setAccounts(accounts);
+  }
+
+  const changeText: React.ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    setMetaData(e.currentTarget.value.trim())
+  }
+
+  const submit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+
+    if (!accounts) {
+      throw new Error('accounts lost')
+    }
+
+    const nonce: number = await web3.eth.getTransactionCount(accounts[0], 'latest');
+
+    //the transaction
+    const tx = {
+      'from': accounts[0],
+      'to': contractAddress,
+      'nonce': nonce.toString(),
+      'gas': '20000000',
+      'data': nftContract.methods.mintNFT(accounts[0], metaData).encodeABI()
+    };
+
+    const gasEstimate = await web3.eth.estimateGas(tx);
+
+    console.log(gasEstimate);
+
+    console.log('making request....');
+
+    const txHash = await (window as any).ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [tx],
+    });
+
+    console.log('***** Success, hash is: ', txHash);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-body">
+        <header className="App-header">
+          <h1 className="App-header-title">On-chain NFT</h1>
+          <div className="App-header-right">
+            {accounts ? <span>you connected</span> : <button className="button" onClick={connectWallet}>Connect wallet</button>}
+          </div>
+        </header>
+        {accounts && (
+          <form onSubmit={submit}>
+            <textarea className="input" onChange={changeText} rows={20} />
+            <button type="submit" className="button">Mint my NFT</button>
+          </form>
+        )}
+      </div>
     </div>
   );
 }
